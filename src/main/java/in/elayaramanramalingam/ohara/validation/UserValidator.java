8e786452e365e2a3e0438c54dd0be0d3f97ed965
Elayaraman-Ramalingam <116268532+Elayaraman-Ramalingam@users.noreply.github.com>
2023-07-25 10:59:23 +0530

    email format validation

--- src/main/java/in/elayaramanramalingam/ohara/validation/UserValidator.java
+++ src/main/java/in/elayaramanramalingam/ohara/validation/UserValidator.java
@@ -1,11 +1,23 @@
 package in.elayaramanramalingam.ohara.validation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import in.elayaramanramalingam.ohara.Util.StringUtil;
 import in.elayaramanramalingam.ohara.exception.ValidationException;
 import in.elayaramanramalingam.ohara.model.User;
 
 public class UserValidator {
+	
+	static void rejectIfInvalidEmail(String email) throws ValidationException {
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
 
+        if (!matcher.matches()) {
+            throw new ValidationException("Invalid email format: " + email);
+        }
+    }
 	public static void validate(User user) throws ValidationException {
 		
 		if(user == null) {
@@ -21,6 +33,7 @@ public class UserValidator {
 //			throw new ValidationException("First name cannot be empty or null");
 //		}
 		StringUtil.rejectIfInvalidString(user.getEmail(),"Email");
+		rejectIfInvalidEmail(user.getEmail());
 		StringUtil.rejectIfInvalidString(user.getPassword(),"password");
 		StringUtil.rejectIfInvalidString(user.getFirstName(),"Firstname");
 		StringUtil.rejectIfInvalidString(user.getLastName(),"Secondname");
